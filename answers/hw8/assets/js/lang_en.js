1d1dbb26a90422d93a81f5f8f76272c68b80976d
lucreciaLin <lsps620@gmail.com>
2017-06-04 18:19:38 +0800

    1. revise layout of keeping new columns on the left when scrolling (I revised .row{});  2. make function changeLang coding style to be more concise of index.js;  3. do web pack on all js files

---
+++ answers/hw8/assets/js/lang_en.js
@@ -0,0 +1,9 @@
+module.exports = !function() {
+	if (!window.I18N) {
+		window.I18N = {};
+	}
+
+	window.I18N["en"] = {
+		TITLE: "The Streams in English"
+	}
+}();
---
+++ answers/hw8/assets/js/lang_zh_tw.js
@@ -0,0 +1,9 @@
+module.exports = !function() {
+	if (!window.I18N) {
+		window.I18N = {};
+	}
+
+	window.I18N["zh-tw"] = {
+		TITLE: "用中文直播的頻道"
+	}
+}();
